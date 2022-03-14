7906cd521f4de5819783249cb3367a2c9e451681
mwaszczuk1 <1marek.waszczuk@gmail.com>
2022-03-14 11:27:02 +0100

    Fix shaky decimal format test

--- formatters/src/main/java/pl/mwaszczuk/formatters/BigNumberFormatter.kt
+++ formatters/src/main/java/pl/mwaszczuk/formatters/BigNumberFormatter.kt
@@ -1,22 +1,27 @@
 package pl.mwaszczuk.formatters
 
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.text.NumberFormat
+import java.util.*
 import kotlin.math.floor
 import kotlin.math.log10
 import kotlin.math.pow
 
 class BigNumberFormatter {
     fun format(number: Double): String {
+        val decimalSymbols = DecimalFormatSymbols.getInstance()
+        decimalSymbols.decimalSeparator = '.'
         val suffix = charArrayOf(' ', 'k', 'M', 'B', 'T', 'P', 'E')
         val numValue = number.toLong()
         val value = floor(log10(numValue.toDouble())).toInt()
         val base = value / 3
         return if (value >= 3 && base < suffix.size) {
-            DecimalFormat("#0.0").format(
+            DecimalFormat("#0.0", decimalSymbols).format(
                 numValue / 10.0.pow((base * 3).toDouble())
             ) + suffix[base]
         } else {
-            DecimalFormat("#,##0").format(numValue)
+            DecimalFormat("#,##0", decimalSymbols).format(numValue)
         }
     }
 }
--- formatters/src/test/java/pl/mwaszczuk/formatters/BigNumberFormatterTest.kt
+++ formatters/src/test/java/pl/mwaszczuk/formatters/BigNumberFormatterTest.kt
@@ -13,8 +13,8 @@ class BigNumberFormatterTest {
         val b = 12345678.0
         val c = 1234567890.0
 
-        Assert.assertEquals("1,0k", formatter.format(a))
-        Assert.assertEquals("12,3M", formatter.format(b))
-        Assert.assertEquals("1,2B", formatter.format(c))
+        Assert.assertEquals("1.0k", formatter.format(a))
+        Assert.assertEquals("12.3M", formatter.format(b))
+        Assert.assertEquals("1.2B", formatter.format(c))
     }
 }
